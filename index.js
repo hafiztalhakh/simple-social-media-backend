c54230751f54e6b82101a672b46e5de2a5c86e6e
Talha Khalid <hafiz.talhakh@gmail.com>
2025-04-15 18:23:42 +0500

    APIs created

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
+import { verifyToken } from "./middleware/auth.js";
+import { createPost } from "./controllers/posts.js";
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -41,9 +45,15 @@ const upload = multer({ storage });
 app.get("/", (req, res) => {
   res.send("Social Media server is running!");
 });
-app.post("/auth/register", upload.single("picture"), register);
 
+/* ROUTES WITH FILES */
+app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
+
+/* ROUTES */
 app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
